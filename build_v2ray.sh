2417eee5ab4467788f23e6b03a79acaf36f95e36
SekiBetu <realsekibetu@gmail.com>
2021-01-16 02:11:48 +0800

    fix typo

--- build_v2ray.sh
+++ build_v2ray.sh
@@ -14,20 +14,20 @@ git clone https://github.com/SekiBetu/v2ray-core.git
 cd v2ray-core || exit 2
 
 echo "Building v2ray_linux_amd64 and v2ctl_linux_amd64"
-env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -ldflags -s -w -o ${cur_dir}/v2ray_linux_amd64 ./main
-env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -ldflags -s -w -tags confonly -o ${cur_dir}/v2ctl_linux_amd64 ./infra/control/main
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -ldflags "-s -w" -o ${cur_dir}/v2ray_linux_amd64 ./main
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -ldflags "-s -w" -tags confonly -o ${cur_dir}/v2ctl_linux_amd64 ./infra/control/main
 
 echo "Building v2ray_linux_arm64 and v2ctl_linux_arm64"
-env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -v -ldflags -s -w -o ${cur_dir}/v2ray_linux_arm64 ./main
-env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -v -ldflags -s -w -tags confonly -o ${cur_dir}/v2ctl_linux_arm64 ./infra/control/main
+env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -v -ldflags "-s -w" -o ${cur_dir}/v2ray_linux_arm64 ./main
+env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -v -ldflags "-s -w" -tags confonly -o ${cur_dir}/v2ctl_linux_arm64 ./infra/control/main
 
 echo "Building v2ray_macos_amd64 and v2ctl_macos_amd64"
-env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -v -ldflags -s -w -o ${cur_dir}/v2ray_macos_amd64 ./main
-env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -v -ldflags -s -w -tags confonly -o ${cur_dir}/v2ctl_macos_amd64 ./infra/control/main
+env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -v -ldflags "-s -w" -o ${cur_dir}/v2ray_macos_amd64 ./main
+env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -v -ldflags "-s -w" -tags confonly -o ${cur_dir}/v2ctl_macos_amd64 ./infra/control/main
 
 echo "Building v2ray_macos_arm64 and v2ctl_macos_arm64"
-env CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -v -ldflags -s -w -o ${cur_dir}/v2ray_macos_arm64 ./main
-env CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -v -ldflags -s -w -tags confonly -o ${cur_dir}/v2ctl_macos_arm64 ./infra/control/main
+env CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -v -ldflags "-s -w" -o ${cur_dir}/v2ray_macos_arm64 ./main
+env CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -v -ldflags "-s -w" -tags confonly -o ${cur_dir}/v2ctl_macos_arm64 ./infra/control/main
 
 echo "Building v2ray.exe and wv2ray.exe and v2ctl.exe"
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o $HOME/v2ray.exe -trimpath -ldflags "-s -w -buildid=" ./main
