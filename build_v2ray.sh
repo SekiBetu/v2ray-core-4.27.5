9dabeddcca1019c510459e713033d5755eb55ab4
SekiBetu <realsekibetu@gmail.com>
2021-02-15 14:43:37 +0800

    Update script and dependencies(include geo data file)

--- build_v2ray.sh
+++ build_v2ray.sh
@@ -1,45 +1,66 @@
 #!/bin/bash
-#
-# This is a Shell script for build multi-architectures v2ray binary file
-# 
-# Supported architectures: amd64, arm32v6, arm32v7, arm64v8, i386, ppc64le, s390x
-# 
-# Copyright (C) 2020 Teddysun <i@teddysun.com>
-#
-# Reference URL:
-# https://github.com/v2fly/v2ray-core.git
 
 cur_dir="$(pwd)"
 
-COMMANDS=( git go )
-for CMD in "${COMMANDS[@]}"; do
-    if [ ! "$(command -v "${CMD}")" ]; then
-        echo "${CMD} is not installed, please install it and try again" && exit 1
-    fi
-done
+echo "删除当前go版本，安装 go 1.16 版本"
+sudo rm -rf /usr/local/go
+wget https://dl.google.com/go/go1.16.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.16.linux-amd64.tar.gz
+sudo rm go1.16.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin'>>~/.bashrc
+export PATH=$PATH:/usr/local/go/bin
+echo "安装 git 7zip"
+sudo apt install -y git
+mkdir 7zip ; cd 7zip/
+wget https://7-zip.org/a/7z2101-linux-x64.tar.xz ; 
+sudo xz -d 7z2101-linux-x64.tar.xz ; sudo tar -xvf 7z2101-linux-x64.tar ; sudo mv 7zz /usr/bin/ ; cd .. ; sudo rm -rf 7zip/
 
 cd ${cur_dir}
-git clone https://github.com/SekiBetu/v2ray-core.git
-cd v2ray-core || exit 2
+git clone https://github.com/SekiBetu/v2ray-core-4.27.5.git
+cd v2ray-core-4.27.5/ || exit 2
+sudo rm go.mod go.sum
+go mod init v2ray.com/core
+go mod tidy
 
-LDFLAGS="-s -w"
-ARCHS=( 386 amd64 arm arm64 ppc64le s390x )
-ARMS=( 6 7 )
+echo "编译 v2ray-linux-64 中"
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -ldflags "-s -w" -o v2ray-linux-64/v2ray ./main
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -ldflags "-s -w" -tags confonly -o v2ray-linux-64/v2ctl ./infra/control/main
+echo "使用 7z 打包中"
+7zz a ${cur_dir}/releases/v2ray-linux-64.7z v2ray v2ctl -mx=9 -r
 
-for ARCH in ${ARCHS[@]}; do
-    if [ "${ARCH}" = "arm" ]; then
-        for V in ${ARMS[@]}; do
-            echo "Building v2ray_linux_${ARCH}${V} and v2ctl_linux_${ARCH}${V}"
-            env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} GOARM=${V} go build -v -ldflags "${LDFLAGS}" -o ${cur_dir}/v2ray_linux_${ARCH}${V} ./main
-            env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} GOARM=${V} go build -v -ldflags "${LDFLAGS}" -tags confonly -o ${cur_dir}/v2ctl_linux_${ARCH}${V} ./infra/control/main
-        done
-    else
-        echo "Building v2ray_linux_${ARCH} and v2ctl_linux_${ARCH}"
-        env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} go build -v -ldflags "${LDFLAGS}" -o ${cur_dir}/v2ray_linux_${ARCH} ./main
-        env CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} go build -v -ldflags "${LDFLAGS}" -tags confonly -o ${cur_dir}/v2ctl_linux_${ARCH} ./infra/control/main
-    fi
-done
+echo "编译 v2ray-linux-arm64-v8a 中"
+cd ${cur_dir}/v2ray-core-4.27.5/
+env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -v -ldflags "-s -w" -o v2ray-linux-arm64-v8a/v2ray ./main
+env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -v -ldflags "-s -w" -tags confonly -o v2ray-linux-arm64-v8a/v2ctl ./infra/control/main
+echo "使用 7z 打包中"
+7zz a ${cur_dir}/releases/v2ray-linux-arm64-v8a.7z v2ray v2ctl -mx=9 -r
 
-chmod +x ${cur_dir}/v2ray_linux_* ${cur_dir}/v2ctl_linux_*
-# clean up
-cd ${cur_dir} && rm -fr v2ray-core
+echo "编译 v2ray-macos-64 中"
+cd ${cur_dir}/v2ray-core-4.27.5/
+env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -v -ldflags "-s -w" -o v2ray-macos-64/v2ray ./main
+env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -v -ldflags "-s -w" -tags confonly -o v2ray-macos-64/v2ctl ./infra/control/main
+echo "使用 7z 打包中"
+7zz a ${cur_dir}/releases/v2ray-macos-64.7z v2ray v2ctl -mx=9 -r
+
+echo "编译 v2ray-macos-arm64 中"
+cd ${cur_dir}/v2ray-core-4.27.5/
+env CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -v -ldflags "-s -w" -o v2ray-macos-arm64-v8a/v2ray ./main
+env CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -v -ldflags "-s -w" -tags confonly -o v2ray-macos-arm64-v8a/v2ctl ./infra/control/main
+echo "使用 7z 打包中"
+7zz a ${cur_dir}/releases/v2ray-macos-arm64-v8a.7z v2ray v2ctl -mx=9 -r
+
+echo "编译 v2ray-windows-64 中"
+cd ${cur_dir}/v2ray-core-4.27.5/
+env CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o v2ray-windows-64/v2ray.exe -trimpath -ldflags "-s -w -buildid=" ./main
+env CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o v2ray-windows-64/wv2ray.exe -trimpath -ldflags "-s -w -H windowsgui -buildid=" ./main
+env CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o v2ray-windows-64/v2ctl.exe -trimpath -ldflags "-s -w -buildid=" -tags confonly ./infra/control/main
+echo "使用 7z 打包中"
+7zz a ${cur_dir}/releases/v2ray-windows-64.7z v2ray.exe wv2ray.exe v2ctl.exe -mx=9 -r
+
+cd ${cur_dir}/v2ray-core-4.27.5/
+echo "移动 go.mod go.sum 至 releases/"
+mv go.mod ${cur_dir}/releases/
+mv go.sum ${cur_dir}/releases/
+cd ${cur_dir}
+echo "清理垃圾"
+sudo rm -rf v2ray-core-4.27.5/ go/
--- go.mod
+++ go.mod
@@ -1,24 +1,23 @@
 module v2ray.com/core
 
-go 1.15
+go 1.16
 
 require (
-	github.com/dgryski/go-metro v0.0.0-20200812162917-85c65e2d0165 // indirect
-	github.com/golang/mock v1.4.4
+	github.com/golang/mock v1.5.0
 	github.com/golang/protobuf v1.4.3
-	github.com/google/go-cmp v0.5.4
+	github.com/google/go-cmp v0.5.5
 	github.com/gorilla/websocket v1.4.2
-	github.com/miekg/dns v1.1.35
-	github.com/pires/go-proxyproto v0.3.3
+	github.com/miekg/dns v1.1.40
+	github.com/pires/go-proxyproto v0.5.0
 	github.com/seiflotfy/cuckoofilter v0.0.0-20201222105146-bc6005554a0c
 	github.com/stretchr/testify v1.7.0
 	github.com/v2fly/VSign v0.0.0-20201108000810-e2adc24bf848
-	go.starlark.net v0.0.0-20201210151846-e81fc95f7bd5
-	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
-	golang.org/x/net v0.0.0-20201224014010-6772e930b67b
-	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a
-	golang.org/x/sys v0.0.0-20201231184435-2d18734c6014
-	google.golang.org/grpc v1.35.0
+	go.starlark.net v0.0.0-20210305151048-6a590ae7f4eb
+	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+	golang.org/x/sys v0.0.0-20210309074719-68d13333faf2
+	google.golang.org/grpc v1.36.0
 	google.golang.org/protobuf v1.25.0
 	h12.io/socks v1.0.2
 )
